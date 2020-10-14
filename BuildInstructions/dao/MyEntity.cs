0a0b57eeda47e701efbf1700df5b0fcbd10e9067
yjj <354291631@qq.com>
2020-10-14 21:25:39 +0800

    第一次提交

--- BuildInstructions/dao/MyEntity.cs
+++ BuildInstructions/dao/MyEntity.cs
@@ -21,6 +21,7 @@
         //为您要在模型中包含的每种实体类型都添加 DbSet。有关配置和使用 Code First  模型
         //的详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=390109。
         public virtual DbSet<Section> Section { get; set; }
+
         public virtual DbSet<ContentTab> ContentTab { get; set; }
         // public virtual DbSet<MyEntity> MyEntities { get; set; }
     }
